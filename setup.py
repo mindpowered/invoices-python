6afe7ba43a1a032eafc625208a0306989c9c5918
Build <support@mindpowered.dev>
2021-01-21 06:38:00 +0000

    Version 0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='mindpowered-invoices',
+    version='0.0.1',
+    author="Mind Powered Corporation",
+    author_email="support@mindpowered.dev",
+    license="CPAL-1.0",
+    url="https://mindpowered.dev/",
+    description="Logic for creating and editing invoices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['invoices'],
+    packages=['mindpowered_invoices'],
+    package_dir={'mindpowered_invoices': 'wrappers'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'mindpowered-maglev',
+    ],
+)
